2ab3c1cd2e014ebae4be99b14a351374ad4fb279
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-04 14:33:43 -0300

    feat: add postCart function

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from "axios";
+import { createAuthHeader } from "../helpers/createAuthHeader";
 import { IFingerprintData } from "../types/cartType";
 import api from "./api";
 
@@ -7,4 +8,11 @@ export async function getFingerprint(): Promise<AxiosResponse<IFingerprintData, 
   const response = await api.get("/cart/fingerprint");
 
   return response.data;
+}
+
+export async function postCart(productId: string) {
+  const config = createAuthHeader();
+  const response = await api.post(`/cart/${productId}`, {}, config);
+
+  return response;
 }
