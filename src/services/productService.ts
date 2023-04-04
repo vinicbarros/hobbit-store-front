74cf6a585b46b8a40fe117570c13269163bd99f7
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-04 19:54:52 -0300

    feat: new functions to api

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -6,3 +6,15 @@ export async function getProducts(): Promise<ProductType[]> {
 
   return response.data as ProductType[];
 }
+
+export async function getProduct(productId: string) {
+  const response = await api.get(`/products/product/${productId}`);
+
+  return response.data as ProductType;
+}
+
+export async function getProductsByCategory(category: string) {
+  const response = await api.get(`/products/${category}`);
+
+  return response.data as ProductType[];
+}
