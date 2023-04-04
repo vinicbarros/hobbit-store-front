20b08a4e3024d4010438a8bf3a84390f70c847b5
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-03 23:02:34 -0300

    feat: add home route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FingerprintRoute from "./common/FingerprintRoute";
+import Home from "./pages/Home";
 import { GlobalStyle } from "./style/GlobalStyle";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route element={<FingerprintRoute />}>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
