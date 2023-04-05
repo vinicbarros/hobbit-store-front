f8709bb783e785d296bf17d4dbf15693aceb34d0
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-05 16:57:57 -0300

    feat: add name under category icon

--- src/components/Category/CategoryMap.tsx
+++ src/components/Category/CategoryMap.tsx
@@ -5,56 +5,71 @@ export default function CategoryMap() {
   const navigate = useNavigate();
   return (
     <Container>
-      <CategoryBox
-        onClick={() => {
-          navigate("/products/Anéis");
-        }}
-      >
-        <Image
-          src="https://i.imgur.com/BJFabUQ.png"
-          alt="Anéis"
-        />
-      </CategoryBox>
-      <CategoryBox
-        onClick={() => {
-          navigate("/products/Armas");
-        }}
-      >
-        <Image
-          src="https://i.imgur.com/0A6bEsD.png"
-          alt="Armas"
-        />
-      </CategoryBox>
-      <CategoryBox
-        onClick={() => {
-          navigate("/products/Artefatos");
-        }}
-      >
-        <Image
-          src="https://i.imgur.com/jTdvg8I.png"
-          alt="Artefatos"
-        />
-      </CategoryBox>
-      <CategoryBox
-        onClick={() => {
-          navigate("/products/Suprimentos");
-        }}
-      >
-        <Image
-          src="https://i.imgur.com/w0cTwIU.png"
-          alt="Suprimentos"
-        />
-      </CategoryBox>
-      <CategoryBox
-        onClick={() => {
-          navigate("/products/Vestuário");
-        }}
-      >
-        <Image
-          src="https://i.imgur.com/Ac6RUan.png"
-          alt="Vestuário"
-        />
-      </CategoryBox>
+      <CategoryInfo>
+        <CategoryBox
+          onClick={() => {
+            navigate("/products/Anéis");
+          }}
+        >
+          <Image
+            src="https://i.imgur.com/BJFabUQ.png"
+            alt="Anéis"
+          />
+        </CategoryBox>
+        <CategoryName>Anéis</CategoryName>
+      </CategoryInfo>
+      <CategoryInfo>
+        <CategoryBox
+          onClick={() => {
+            navigate("/products/Armas");
+          }}
+        >
+          <Image
+            src="https://i.imgur.com/0A6bEsD.png"
+            alt="Armas"
+          />
+        </CategoryBox>
+        <CategoryName>Armas</CategoryName>
+      </CategoryInfo>
+      <CategoryInfo>
+        <CategoryBox
+          onClick={() => {
+            navigate("/products/Artefatos");
+          }}
+        >
+          <Image
+            src="https://i.imgur.com/jTdvg8I.png"
+            alt="Artefatos"
+          />
+        </CategoryBox>
+        <CategoryName>Artefatos</CategoryName>
+      </CategoryInfo>
+      <CategoryInfo>
+        <CategoryBox
+          onClick={() => {
+            navigate("/products/Suprimentos");
+          }}
+        >
+          <Image
+            src="https://i.imgur.com/w0cTwIU.png"
+            alt="Suprimentos"
+          />
+        </CategoryBox>
+        <CategoryName>Suprimentos</CategoryName>
+      </CategoryInfo>
+      <CategoryInfo>
+        <CategoryBox
+          onClick={() => {
+            navigate("/products/Vestuário");
+          }}
+        >
+          <Image
+            src="https://i.imgur.com/Ac6RUan.png"
+            alt="Vestuário"
+          />
+        </CategoryBox>
+        <CategoryName>Vestuário</CategoryName>
+      </CategoryInfo>
     </Container>
   );
 }
@@ -63,10 +78,25 @@ const Container = styled.section`
   width: 90%;
   margin-inline: auto;
   margin-top: 20px;
-  padding-bottom: 40px;
+  padding-bottom: 30px;
   display: flex;
 
   overflow: scroll;
+`;
+
+const CategoryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & + & {
+    margin-left: 20px;
+  }
+`;
+
+const CategoryName = styled.h4`
+  margin-top: 10px;
+  font-size: 12px;
 `;
 
 const CategoryBox = styled.div`
@@ -77,9 +107,6 @@ const CategoryBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  & + & {
-    margin-left: 20px;
-  }
 
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   cursor: pointer;
